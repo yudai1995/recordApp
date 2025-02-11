458e4f0ff0324912f167ef7e6575010047148439
yamamoto <y.yamamoto991103@gmail.com>
2025-02-12 06:38:29 +0900

    update model

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RecordModel } from '../model/record.model';
 import { RecordService } from '../service/record.service';
+import { Status } from '../model/status';
 
 @Component({
   selector: 'app-dashbord',
@@ -16,15 +17,21 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
     this.getRecords();
   }
-
   private getRecords(): void {
-    this.recordService.getEntitys<RecordModel>().subscribe({
-      next: (record: RecordModel[]) => (this.records = record.slice(0, 5)),
-      error: (error: any) => {
-        console.error('HTTPリクエストエラー:', error);
-        this.error = 'データの取得に失敗しました。';
-      },
-      complete: () => console.info(this.records),
-    });
+    this.recordService
+      .getEntitys<Status & { records: RecordModel[] }>()
+      .subscribe({
+        next: (results) => {
+          // 各結果のrecordsプロパティをフラット化して1つの配列にまとめる
+          this.records = results
+            .flatMap((result) => result.records)
+            .slice(0, 5);
+        },
+        error: (error: any) => {
+          console.error('HTTPリクエストエラー:', error);
+          this.error = 'データの取得に失敗しました。';
+        },
+        complete: () => console.info(this.records),
+      });
   }
 }
